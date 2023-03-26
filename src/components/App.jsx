6f73f8bd1dfa55cc6069ac4f28f12a66fe5bd11e
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-03-26 16:01:31 +0200

    f

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable array-callback-return */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useEffect, useState, useRef } from 'react';
 import { nanoid } from 'nanoid';
 
